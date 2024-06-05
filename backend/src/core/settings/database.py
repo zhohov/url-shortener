13d8c82f49480c431582ac948fe7ad79e784faaa
maksimzhohov <zhohovmaksim@gmail.com>
2024-06-06 01:31:17 +0300

    Added postgres

--- backend/src/core/settings/database.py
+++ backend/src/core/settings/database.py
@@ -1,12 +1,15 @@
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-from .common import BASE_DIR
-
+import os
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
     }
 }
